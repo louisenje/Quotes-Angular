5b7318cb8c4816ee92410ea18c59122b559e6274
louisenje <louisenje@gmail.com>
2020-04-02 08:21:48 +0300

    added highlight element background colour green

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef) {}
+   highlightElement(){
+    this.el.nativeElement.style.backgroundColor = 'green';
+
+   }
+   ngOnInit(){
+    this.highlightElement();
+   }
 
 }
+

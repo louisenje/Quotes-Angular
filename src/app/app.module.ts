471bbe2f0c1d59d7d7feb298f9b9b221948b74ab
louisenje <louisenje@gmail.com>
2020-04-02 08:18:57 +0300

    generated a directive highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-//import { DateCountPipe } from './date-count.pipe';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 //import { HighlightDirective  } from './highlight.directive'
 
 
@@ -15,10 +14,10 @@ import { DateCountPipe } from './date-count.pipe';
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-   // DateCountPipe,
+    
     QuoteFormComponent,
-   DateCountPipe,
-   // HighlightDirective 
+    DateCountPipe,
+    HighlightDirective 
   ],
   imports: [
     BrowserModule,
